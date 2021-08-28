0e108859b2c0968cae9e0dc88ba58f79dd479e1e
andrerj2000 <andrerj2000@gmail.com>
2021-08-28 10:38:18 -0300

    Carrega o primeiro commit do projeto para GitHub

---
+++ personApp/src/test/java/one/digitalInovation/personApp/PersonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInovation.personApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
